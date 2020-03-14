e5f12affdbdd630cf21166383b0944b2332c6028
gaoqi <gaoqi198@163.com>
2020-03-14 22:26:49 +0800

    feat: registerdb 重构

---
+++ components/db.go
@@ -0,0 +1,44 @@
+package components
+
+import (
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/config"
+	"strings"
+	"fmt"
+)
+
+func InitDB()  {
+	var err error
+	BConfig, err := config.NewConfig("ini", "conf/app.conf")
+	if err != nil{
+			fmt.Println("config init error:", err)
+	}
+
+	dbuser := BConfig.String("dev::dbuser")
+	dbpwd := BConfig.String("dev::dbpass")
+	dbhost := BConfig.String("dev::dbhost")
+	aedbname := BConfig.String("dev::aedbname")
+	dbport := BConfig.String("dev::dbport")
+
+	aeDsn := genDsn(dbuser,dbpwd,dbhost,dbport,aedbname)
+	exdbname := BConfig.String("dev::exdbname")
+	exDsn :=genDsn(dbuser,dbpwd,dbhost,dbport,exdbname)
+
+	orm.Debug = true
+	dbdriver := BConfig.String("dev::dbdriver")
+	orm.RegisterDataBase("default", dbdriver, aeDsn, 30)
+	orm.RegisterDataBase("exchange", dbdriver, exDsn, 30)
+}
+
+func genDsn(dbuser,dbpwd,dbhost,dbport,dbname string) string {
+
+	ds := make([]string, 0)
+	ds = append(ds, dbuser + ":")
+	ds = append(ds, dbpwd + "@tcp(")
+	ds = append(ds, dbhost + ":")
+	ds = append(ds, dbport + ")/")
+	ds = append(ds, dbname + "?charset=utf8")
+
+	datasourcename := strings.Join(ds, "")
+	return datasourcename
+}
--- main.go
+++ main.go
@@ -3,51 +3,12 @@ package main
 import (
 	_ "template/routers"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
-	"github.com/astaxie/beego/config"
 	"template/components"
-	"strings"
-	"fmt"
 )
 
-var BConfig config.Configer
-
 func init() {
-	var err error
-	BConfig, err = config.NewConfig("ini", "conf/app.conf")
-	if err != nil{
-			fmt.Println("config init error:", err)
-	}
-
-	dbuser := BConfig.String("dev::dbuser")
-	dbpwd := BConfig.String("dev::dbpass")
-	dbhost := BConfig.String("dev::dbhost")
-	aedbname := BConfig.String("dev::aedbname")
-	dbport := BConfig.String("dev::dbport")
-
-	aeDsn := genDsn(dbuser,dbpwd,dbhost,dbport,aedbname)
-	exdbname := BConfig.String("dev::exdbname")
-	exDsn :=genDsn(dbuser,dbpwd,dbhost,dbport,exdbname)
-
-	orm.Debug = true
-	dbdriver := BConfig.String("dev::dbdriver")
-	orm.RegisterDataBase("default", dbdriver, aeDsn, 30)
-	orm.RegisterDataBase("exchange", dbdriver, exDsn, 30)
-
+	components.InitDB()
 	components.InitLogger()
-}
-
-func genDsn(dbuser,dbpwd,dbhost,dbport,dbname string) string {
-
-	ds := make([]string, 0)
-	ds = append(ds, dbuser + ":")
-	ds = append(ds, dbpwd + "@tcp(")
-	ds = append(ds, dbhost + ":")
-	ds = append(ds, dbport + ")/")
-	ds = append(ds, dbname + "?charset=utf8")
-
-	datasourcename := strings.Join(ds, "")
-	return datasourcename
 }
 
 func main() {
